dcaabd866270399a156cb4ae7b89dd69be9c7c8e
riguang <riguangjin77@gmail.com>
2022-01-06 08:17:58 -0500

    adding update and delete.

--- hibernate-1-4/src/main/java/org/example/Application.java
+++ hibernate-1-4/src/main/java/org/example/Application.java
@@ -10,26 +10,56 @@ public class Application {
 
         Session session = HibernateUtil.getSessionFactory().openSession();
 
+        // insert
         session.beginTransaction();
 
         session.save(new Student("Bosco", "bosco@email.com"));
         session.save(new Student("Kevin", "kevin@email.com"));
         System.out.println("saved --");
-
         session.getTransaction().commit();
 
+
+        // select
         session.beginTransaction();
 
-        List<Student> result = session.createQuery( "from Student" ).list();
+        List result = session.createQuery( "from Student" ).list();
 
-        for ( Student student : result ) {
+        for ( Student student : (List<Student>)result ) {
             System.out.println( "Student (Id: " + student.getStudentId()+ " - "
                     + student.getName() + ") : "
                     + student.getEmail());
         }
         session.getTransaction().commit();
-        session.close();
 
+
+        // update
+        session.beginTransaction();
+
+        int updatedEntities = session.createQuery(
+                "update Student " +
+                        "set email = :newEmail " +
+                        "where name = :oldName" )
+                .setParameter("oldName", "Bosco")
+                .setParameter("newEmail", "new_bosco@email.com")
+                .executeUpdate();
+
+        System.out.println("updated: " + updatedEntities);
+        session.getTransaction().commit();
+
+
+        // delete
+        session.beginTransaction();
+
+        int deletedRows = session.createQuery(
+                "delete Student " +
+                        "where name = :name" )
+                .setParameter("name", "Kevin")
+                .executeUpdate();
+
+        System.out.println("deleted: " + deletedRows);
+        session.getTransaction().commit();
+
+        session.close();
 
         HibernateUtil.shutdown();
     }
